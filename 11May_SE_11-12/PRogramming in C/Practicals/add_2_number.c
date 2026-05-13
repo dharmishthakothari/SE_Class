54281608fb2f9faa68b975488c8c6872328f17ef
dharmishthakothari <dharmishtha.only@gmail.com>
2026-05-13 12:01:07 +0530

    Add files via upload

---
+++ 11May_SE_11-12/PRogramming in C/Practicals/add_2_number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	//int --- data type 
+	// num1 & num2 --- variable 
+	int num1=201,num2=300;
+	
+	int ans;
+	ans=num1+num2;
+	printf("Addition = %d",ans);
+	ans=num1-num2;
+	printf("\n\n\nSubstraction = %d",ans);
+}
